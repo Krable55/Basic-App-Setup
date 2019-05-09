01d70beec9be1c70764e4c208b98b7af832fc1d0
Christian Carter <cacbcarter68@gmail.com>
2019-05-08 20:36:56 -0400

    workouts model finished

--- server/databases/postgres/models/Workouts.js
+++ server/databases/postgres/models/Workouts.js
@@ -1,10 +1,17 @@
-
-
 module.exports = function workoutModel(sequelize, DataTypes) {
     const Workouts = sequelize.define(
         'Workouts',
         {
-            name: DataTypes.STRING,
+            id: {
+                primaryKey: true,
+                allowNull: false,
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+            },
+            exercise: DataTypes.STRING,
+            userId: DataTypes.STRING,
+            reps: DataTypes.INTEGER,
+            weight: DataTypes.INTEGER,
         },
         {
             tableName: 'workouts',
@@ -15,3 +22,5 @@ module.exports = function workoutModel(sequelize, DataTypes) {
 
     return Workouts;
 };
+
+// id, userId, exercise, reps, weight
